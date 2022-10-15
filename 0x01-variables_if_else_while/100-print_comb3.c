31e2ae3bcf8f11bc9cdf8a403d880e14ee7aa2cc
Gsamson5 <gloryadewopo@gmail.com>
2022-10-15 13:58:43 -0700

    100-if else

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints numbers of different combinations
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, i;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (a < i)
+			{
+				putchar(a);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
